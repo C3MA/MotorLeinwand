c5cea68b7d0ebb174b1fa8392d1ff49088e01444
c3ma <c3ma@calculator>
2014-05-17 15:07:58 +0200

    Working State

--- Firmware/RemoteMaster/spi_master.c
+++ Firmware/RemoteMaster/spi_master.c
@@ -11,25 +11,28 @@ Verdrahtung:	MISO(Master) --> MISO(Slave)
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#define BUTTON_UP	PD2	/* Button to send an "Up SPI signal" */	
-#define BUTTON_DOWN	PD3	/* Button to send an "Down SPI signal" */	
-#define BUTTON_STOP	PD4	/* Button to send an "Stop SPI signal" */	
-
-#define DIAGNOSE_LED	PD5	/* Nur zu debug Zwecken */
-
 unsigned char status = 0;
-volatile unsigned char count = 0;
+volatile unsigned char count;
+
+void timer1 (void);
+void master_init (void);
+void master_transmit (unsigned char data);
+
+/*ISR names found in avr/iom8.h */
+ISR (SPI_STC_vect) {
+	return;
+}
 
 /*ISR names found in avr/iom8.h */
 ISR (TIMER1_OVF_vect) {						//Senderoutine
 	if (count == 1) {
+		master_transmit ('1');
 		count--;
-		master_transmit('0');
-		PORTD &= ~(1<<DIAGNOSE_LED);
+		PORTD = 0x0;
 	} else if (count == 0) {
+		master_transmit ('0');
 		count++;
-		master_transmit('1');
-		PORTD |= (1<<DIAGNOSE_LED);
+		PORTD = 0xff;
 	}
 }
 
@@ -40,7 +43,7 @@ void timer1 (void) {
 }
 
 void master_init (void) {
-	DDRB = (1<<PB2) | (1<<PB3) | (1<<PB5);		// setze SCK,MOSI,PB2 (SS) als Ausgang
+	DDRB = (1<<PB0) | (1<<PB3) | (1<<PB5);		// setze SCK,MOSI,PB0 (SS) als Ausgang
 	DDRB &= ~(1<<PB4);							// setze MISO als Eingang
 	PORTB = (1<<PB5) | (1<<PB2);				// SCK und PB2 high (ist mit SS am Slave verbunden)
 	SPCR = (1<<SPE) | (1<<MSTR) | (1<<SPR0);	//Aktivierung des SPI, Master, Taktrate fck/16
@@ -48,15 +51,15 @@ void master_init (void) {
 }
 
 void master_transmit (unsigned char data) {
-	PORTB &= ~(1<<PB2);						//SS am Slave Low --> Beginn der Übertragung
+	PORTB &= ~(1<<PB0);						//SS am Slave Low --> Beginn der Übertragung
 	SPDR = data;								//Schreiben der Daten
 	while (!(SPSR & (1<<SPIF)));
-	PORTB |= (1<<PB2);							//SS High --> Ende der Übertragung
+	PORTB |= (1<<PB0);							//SS High --> Ende der Übertragung
 }
 
 int main (void) {
-	DDRD = 0;
-	DDRD |= (1<<DIAGNOSE_LED);   // passende Pins als Ausgang markieren
+	DDRD = 0xff;		// PortD als Ausgang nutzen (Chris & Ollo geraten)
+	master_init ();
 	timer1 ();
 	sei ();
 
