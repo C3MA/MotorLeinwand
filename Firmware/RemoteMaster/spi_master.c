5c69eedf4ec6666ec86be097de2dd340348c5d5e
c3ma <c3ma@calculator>
2014-05-17 15:09:21 +0200

    Working State

--- Firmware/RemoteMaster/spi_master.c
+++ Firmware/RemoteMaster/spi_master.c
@@ -5,7 +5,7 @@ LEDs an bzw. aus gehen
 Verdrahtung:	MISO(Master) --> MISO(Slave)
 				MOSI(Master) --> MOSI(Slave)
 				SCK(Master)  --> SCK(Slave)
-				PB0(Master)	 --> SS(Slave)
+				PB2(Master)	 --> SS(Slave)
 **************************************************************/
 
 #include <avr/io.h>
@@ -26,11 +26,11 @@ ISR (SPI_STC_vect) {
 /*ISR names found in avr/iom8.h */
 ISR (TIMER1_OVF_vect) {						//Senderoutine
 	if (count == 1) {
-		master_transmit ('1');
+		master_transmit ('D');
 		count--;
 		PORTD = 0x0;
 	} else if (count == 0) {
-		master_transmit ('0');
+		master_transmit ('U');
 		count++;
 		PORTD = 0xff;
 	}
@@ -43,7 +43,7 @@ void timer1 (void) {
 }
 
 void master_init (void) {
-	DDRB = (1<<PB0) | (1<<PB3) | (1<<PB5);		// setze SCK,MOSI,PB0 (SS) als Ausgang
+	DDRB = (1<<PB2) | (1<<PB3) | (1<<PB5);		// setze SCK,MOSI,PB2 (SS) als Ausgang
 	DDRB &= ~(1<<PB4);							// setze MISO als Eingang
 	PORTB = (1<<PB5) | (1<<PB2);				// SCK und PB2 high (ist mit SS am Slave verbunden)
 	SPCR = (1<<SPE) | (1<<MSTR) | (1<<SPR0);	//Aktivierung des SPI, Master, Taktrate fck/16
@@ -51,14 +51,14 @@ void master_init (void) {
 }
 
 void master_transmit (unsigned char data) {
-	PORTB &= ~(1<<PB0);						//SS am Slave Low --> Beginn der Übertragung
+	PORTB &= ~(1<<PB2);						//SS am Slave Low --> Beginn der Übertragung
 	SPDR = data;								//Schreiben der Daten
 	while (!(SPSR & (1<<SPIF)));
-	PORTB |= (1<<PB0);							//SS High --> Ende der Übertragung
+	PORTB |= (1<<PB2);							//SS High --> Ende der Übertragung
 }
 
 int main (void) {
-	DDRD = 0xff;		// PortD als Ausgang nutzen (Chris & Ollo geraten)
+	DDRD = 0xff;		// PortD als Ausgang nutzen 
 	master_init ();
 	timer1 ();
 	sei ();
