b8e68289c24340fca93b505efd39e56bc5843f65
c3ma <c3ma@calculator>
2014-05-23 15:55:17 +0200

    formatting

--- Firmware/RemoteMaster/spi_master.c
+++ Firmware/RemoteMaster/spi_master.c
@@ -70,9 +70,9 @@ ISR (INT0_vect) {
 }
 
 void timer1 (void) {
-	TIMSK |= (1<<TOIE1);           				//Timer Overflow Interrupt enable
-	TCNT1 = 0;                					//Rücksetzen des Timers
-	TCCR1B = (1<<CS10);			//8MHz/65536/64 = 1,91Hz --> 0,5s
+	TIMSK |= (1<<TOIE1);    //Timer Overflow Interrupt enable
+	TCNT1 = 0;       	//Rücksetzen des Timers
+	TCCR1B = (1<<CS10);	//8MHz/65536/64 = 1,91Hz --> 0,5s
 }
 
 
@@ -82,17 +82,17 @@ void master_init (void) {
 	DDRD = 0;		// PortD alles Eingang
 	DDRD |= (1<<LED);	// LED als Ausgang (Dev)
 	DDRB = (1<<PB2) | (1<<PB3) | (1<<PB5);		// setze SCK,MOSI,PB2 (SS) als Ausgang
-	DDRB &= ~(1<<PB4);							// setze MISO als Eingang
-	PORTB = (1<<PB5) | (1<<PB2);				// SCK und PB2 high (ist mit SS am Slave verbunden)
+	DDRB &= ~(1<<PB4);				// setze MISO als Eingang
+	PORTB = (1<<PB5) | (1<<PB2);			// SCK und PB2 high (ist mit SS am Slave verbunden)
 	SPCR = (1<<SPE) | (1<<MSTR) | (1<<SPR0);	//Aktivierung des SPI, Master, Taktrate fck/16
 	status = SPSR;								//Status löschen
 }
 
 void master_transmit (unsigned char data) {
-	PORTB &= ~(1<<PB2);						//SS am Slave Low --> Beginn der Übertragung
-	SPDR = data;								//Schreiben der Daten
+	PORTB &= ~(1<<PB2);			//SS am Slave Low --> Beginn der Übertragung
+	SPDR = data;				//Schreiben der Daten
 	while (!(SPSR & (1<<SPIF)));
-	PORTB |= (1<<PB2);							//SS High --> Ende der Übertragung
+	PORTB |= (1<<PB2);			//SS High --> Ende der Übertragung
 }
 
 void int0_init (void) {
