ec103fcf728ae8ad1779c7e05938a5a79df34247
ollo <ollo@olloThinkPad>
2016-05-20 20:46:13 +0200

    The main file is tested before executed

--- Firmware/init.lua
+++ Firmware/init.lua
@@ -1,6 +1,10 @@
 -- Delay the boot
 
 tmr.alarm(2, 1000, tmr.ALARM_SINGLE, function()
-    dofile("main.lua")
+    if (file.open("main.lua")) then
+        dofile("main.lua")
+    else
+        print("No Main file found")
+    end
 end)
 print("Autostart in 1 Second")
